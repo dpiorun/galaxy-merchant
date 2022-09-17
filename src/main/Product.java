1436f769aeaf00c57ef32f56c0d733d03f134dec
Damian Piorun <piorun@bakboka.com>
2022-09-17 18:14:22 +0200

    add Product feature

--- src/main/Product.java
+++ src/main/Product.java
@@ -1,4 +1,22 @@
 package main;
 
 public class Product {
+    String name;
+    Integer unitPrice;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getUnitPrice() {
+        return this.unitPrice;
+    }
+
+    public void setUnitPrice(int price) {
+        this.unitPrice = price;
+    }
 }
