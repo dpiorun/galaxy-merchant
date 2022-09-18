fa120e2f2cb6df2593aa2c5893cd172fe265b2c8
Damian Piorun <piorun@bakboka.com>
2022-09-18 15:32:46 +0200

    feature: add notes scrapper
    NotesScrapper collects information from notes and answers questions.

---
+++ src/main/NotesScrapper.java
@@ -0,0 +1,97 @@
+package main;
+
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NotesScrapper {
+    private IntergalacticalParser parser;
+    private HashMap<String, Product> products;
+    private String ultimateStatement = "I have no idea what you are talking about";
+    private String note;
+
+    public NotesScrapper(IntergalacticalParser parser, HashMap<String, Product> productStore) {
+        products = productStore;
+        this.parser = parser;
+    }
+
+    public void scrap(String note) {
+        this.note = note.trim().replaceAll("\\s+", " ");
+        boolean itIsAQuestion = this.note.matches(".+\\?$");
+        if (itIsAQuestion) this.handleQuestion();
+        else this.handleData();
+    }
+
+    private Matcher createMatcher(String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(this.note);
+    }
+
+    private void handleData() {
+        // ex. match: "glob is I"
+        Matcher matcher = createMatcher("(?<intergalacticalNumeral>\\w+)(\\s+is\\s+)(?<romanNumeral>[MDCLXVI]$)");
+        if (matcher.matches()) {
+            String intergalacticalNumeral = matcher.group("intergalacticalNumeral");
+            String romanNumeral = matcher.group("romanNumeral");
+
+            this.parser.set(intergalacticalNumeral, romanNumeral.charAt(0));
+            return;
+        }
+
+        // ex. match "glob glob Silver is 34 Credits"
+        matcher = createMatcher("(?<numeral>([a-zA-Z]+\\s*?\\b)*)(\\s+)(?<product>\\w+)(\\s+is\\s+)(?<price>[0-9]+)(\\s+Credits)");
+        if (matcher.matches()) {
+            String name = matcher.group("product");
+            String numeral = matcher.group("numeral");
+            Integer price = Integer.parseInt(matcher.group("price"));
+
+            try {
+                Integer unitPrice = price / parser.toTerrestrial(numeral);
+                Product product = new Product(name);
+                product.setUnitPrice(unitPrice);
+                products.put(name, product);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            return;
+        }
+
+        System.out.println(ultimateStatement);
+    }
+
+    private void handleQuestion() {
+        // ex. match "how much is pish tegj glob glob ?"
+        Matcher matcher = createMatcher("(how\\s+much\\s+is\\s+)(?<numeral>([a-zA-Z]+\\s*?\\b)*)(\\s*?\\?$)");
+        if (matcher.matches()) {
+            try {
+                String intergalacticalNumeral = matcher.group("numeral");
+                int parsedNumeral = parser.toTerrestrial(intergalacticalNumeral);
+
+                System.out.println(intergalacticalNumeral + " is " + parsedNumeral);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            return;
+        }
+
+        // ex. match "how many Credits is pish tegj glob glob Silver ?"
+        matcher = createMatcher("(how\\s+many\\s+Credits\\s+is\\s+)(?<numeral>([a-zA-Z]+\\s+?\\b)*)(?<product>[a-zA-Z]+)(\\s*?\\?$)");
+        if (matcher.matches()) {
+            try {
+                String numeral = matcher.group("numeral").trim();
+                int quantity = parser.toTerrestrial(numeral);
+
+                String productName = matcher.group("product");
+                Product queriedProduct = products.get(productName);
+                int price = queriedProduct.getUnitPrice() * quantity;
+
+                System.out.println(numeral + " " + productName + " is " + price + " Credits");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            return;
+        }
+
+        System.out.println(ultimateStatement);
+    }
+}
---
+++ src/test/NotesScrapperTest.java
@@ -0,0 +1,119 @@
+package test;
+
+import main.IntergalacticalParser;
+import main.NotesScrapper;
+import main.Product;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+class NotesScrapperTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private IntergalacticalParser parser;
+    private HashMap<String, Product> products;
+    private NotesScrapper scrapper;
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+        parser = spy(new IntergalacticalParser());
+        products = new HashMap();
+        scrapper = new NotesScrapper(parser, products);
+    }
+
+    private void setupParser() {
+        parser.set("glob", 'I');
+        parser.set("prok", 'V');
+        parser.set("pish", 'X');
+        parser.set("tegj", 'L');
+    }
+
+    private void setupProduct() {
+        products.put("Test", new Product("Test"));
+        products.get("Test").setUnitPrice(10);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "glob is I",
+            "prok is V",
+            "  pish is   X  ",
+            "tegj is L",
+    })
+    void itAddsTranslationsToIntergalacticalParser(String note) {
+        note = note.trim();
+        scrapper.scrap(note);
+        String[] values = note.split("\\s+is\\s+");
+        verify(parser).set(values[0], values[1].charAt(0));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "glob glob Silver is 34 Credits",
+            "glob prok Gold is 57800 Credits",
+            "  pish pish Iron is  3910   Credits",
+    })
+    void itAddsProductAndItsPriceFromANote(String note) {
+        note = note.trim();
+        String[] values = note.split("\\s+is\\s+");
+        String[] firstPart = values[0].split("\\s+");
+        String productName = firstPart[firstPart.length - 1];
+        setupParser();
+        scrapper.scrap(note);
+
+        assertTrue(products.containsKey(productName));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "how much is prok ?,                        prok is 5",
+            "how  much  is  pish  tegj  glob glob?,     pish tegj glob glob is 42",
+    })
+    void itTranslatesIntergalacticalNumerals(String note, String answer) {
+        setupParser();
+
+        scrapper.scrap(note);
+        assertEquals(answer, outputStreamCaptor.toString().trim());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "how many Credits is glob prok Test ?,    glob prok Test is 40 Credits",
+            "  how  many  Credits  is  prok  Test?,   prok Test is 50 Credits",
+    })
+    void itCalculatesThePriceOfAQuery(String note, String answer) {
+        setupParser();
+        setupProduct();
+
+        scrapper.scrap(note);
+        assertEquals(answer, outputStreamCaptor.toString().trim());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "how much wood could a woodchuck chuck if a woodchuck could chuck wood ?"
+    })
+    void itAdmitsItsIgnoranceWhenApplicable(String note) {
+        setupParser();
+
+        scrapper.scrap(note);
+        assertEquals("I have no idea what you are talking about", outputStreamCaptor.toString().trim());
+    }
+}
